d936f53ae422fab360f5e1cd62f7114d91ba270c
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2015-12-02 01:36:07 +0100

    Add lagged phase space plot

--- extra_plots.py
+++ extra_plots.py
@@ -6,6 +6,8 @@ import sys
 import numpy as np
 import matplotlib.pylab as plt
 
+from singularity_finder import compute_tau
+
 
 def neural_spike():
     """ Plot various neural spikes
@@ -30,6 +32,36 @@ def neural_spike():
         for i in range(height):
             do_plot(camp[i, j])
 
+def lagged_phase_space():
+    """ Plot phase space lagged by tau
+    """
+    def do_plot(cell_evo):
+        x = []
+        y = []
+        for t in range(len(cell_evo) - tau):
+            x.append(cell_evo[t + tau])
+            y.append(cell_evo[t])
+
+        plt.plot(x, y)
+
+        plt.title(r'Lagged phase space of neural spike ($\tau = %d$)' % tau)
+        plt.xlabel(r'$x_{ij}(t - \tau)$')
+        plt.ylabel(r'$x_{ij}(t)$')
+
+        #plt.savefig('images/lagged_phase_space.png', bbox_inches='tight', dpi=300)
+        plt.show()
+
+    camp, pacemaker = np.load(sys.argv[1])
+    camp = np.rollaxis(camp, 0, 3)
+
+    tau = compute_tau(camp)
+
+    width, height, depth = camp.shape
+    for j in range(width):
+        for i in range(height):
+            do_plot(camp[i, j])
+
 
 if __name__ == '__main__':
-    neural_spike()
+    #neural_spike()
+    lagged_phase_space()
